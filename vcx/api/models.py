4dca1e2455ca4cec95c6cf14ed7c7b5b212f4dd2
Arthur Schurhaus <arthurschur@icloud.com>
2024-09-20 19:05:03 -0300

    Added book and Author Models

--- vcx/api/models.py
+++ vcx/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    edition = models.CharField(max_length=100)
+    year_published = models.IntegerField()
+    authors = models.ManyToManyField(Author) #django ja relaciona automaticamente para mim
+
